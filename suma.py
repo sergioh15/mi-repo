c87a597e0c9345a856bcb73d421524c3749d7da6
sergioh15 <sergio.heredia01@correo.usa.edu.co>
2024-08-27 14:47:30 +0000

    Inicializando repositorio con archivos existentes

---
+++ suma.py
@@ -0,0 +1,10 @@
+# suma.py
+def suma(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    x = 5
+    y = 3
+    print(f"La suma de {x} y {y} es {suma(x, y)}")
+
+
---
+++ venv/suma.py
@@ -0,0 +1,9 @@
+# suma.py
+def sumar(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    a = 5
+    b = 3
+    resultado = sumar(a, b)
+    print(f"La suma de {a} y {b} es {resultado}")
