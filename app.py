f8e26c2eb41d0417b72801ad428a1e31613de701
sergioh15 <sergio.heredia01@correo.usa.edu.co>
2024-08-29 19:14:54 +0000

    Modificado app.py para decir 'Hola Sergio desde Cloud 9'

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hola Mundo Sergio'
+    return 'Hola Sergio desde Cloud 9'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
+
+
 
