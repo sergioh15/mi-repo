5303846d422b11b40b3b97e8f96e8bdf401b2186
sergioh15 <sergio.heredia01@correo.usa.edu.co>
2024-08-29 15:25:16 +0000

    Add requirements.txt add Flask

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def hello_world():
     return 'Hola Mundo Sergio'
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(host='0.0.0.0', port=8080, debug=True)
